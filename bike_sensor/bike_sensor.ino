bd30d91274c1730fff1214bedb799216d3089931
Nam Ngo <nam.ngo.shop@gmail.com>
2020-03-31 12:57:30 -0700

    changes

---
+++ bike_sensor/bike_sensor.ino
@@ -0,0 +1,19 @@
+#include <samd.h>
+
+#define LED_BUILTIN 38
+
+void setup() {
+  while (!SerialUSB)
+    ;
+  SerialUSB.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  SerialUSB.println("hey yo1");
+  digitalWrite(LED_BUILTIN,
+               HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(400);          // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+  delay(400);                      // wait for a second
+}
