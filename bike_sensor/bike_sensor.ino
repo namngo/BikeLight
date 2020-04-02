e2ae1b972112e26ec5ff1862a412e562e09f842a
Nam Ngo <namngo@google.com>
2020-04-01 17:47:24 -0700

    library

--- bike_sensor/bike_sensor.ino
+++ bike_sensor/bike_sensor.ino
@@ -1,19 +1,19 @@
+#include <ICM_20948.h>
 #include <samd.h>
+#include <Wire.h>
 
 #define LED_BUILTIN 38
 
 void setup() {
-  while (!SerialUSB)
-    ;
   SerialUSB.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  SerialUSB.println("hey yo1");
+  SerialUSB.println("hey yo2");
   digitalWrite(LED_BUILTIN,
                HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(400);          // wait for a second
+  delay(800);          // wait for a second
   digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(400);                      // wait for a second
+  delay(800);
 }
