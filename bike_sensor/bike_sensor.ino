0d388c5a72cdbe0c123ac5c7e5c934917d5dd618
namngo <ngoquangnam2006@gmail.com>
2020-04-12 20:32:04 -0700

    change

--- bike_sensor/bike_sensor.ino
+++ bike_sensor/bike_sensor.ino
@@ -1,129 +1,50 @@
 #include <Adafruit_BME280.h>
 #include <MPU9250.h>
 #include <Wire.h>
-#include <quaternionFilters.h>
 #include <samd.h>
 
 #define I2C_CLOCK 400000
-// #define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
-#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0
 #define BME280_ADDR 0x76  // or 0x77
 #define LED_BUILTIN 38
 
-MPU9250 myIMU(MPU9250_ADDRESS, Wire, I2C_CLOCK);
-
-void InitIMU(Serial_ Serial, MPU9250 myIMU) {
-  byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-  Serial.print(F("MPU9250 I AM 0x"));
-  Serial.print(c, HEX);
-  if (c == 0x71)  // WHO_AM_I should always be 0x71
-  {
-    Serial.println(F("MPU9250 is online..."));
-
-    // Start by performing self test and reporting values
-    myIMU.MPU9250SelfTest(myIMU.selfTest);
-    Serial.print(F("x-axis self test: acceleration trim within : "));
-    Serial.print(myIMU.selfTest[0], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("y-axis self test: acceleration trim within : "));
-    Serial.print(myIMU.selfTest[1], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("z-axis self test: acceleration trim within : "));
-    Serial.print(myIMU.selfTest[2], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("x-axis self test: gyration trim within : "));
-    Serial.print(myIMU.selfTest[3], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("y-axis self test: gyration trim within : "));
-    Serial.print(myIMU.selfTest[4], 1);
-    Serial.println("% of factory value");
-    Serial.print(F("z-axis self test: gyration trim within : "));
-    Serial.print(myIMU.selfTest[5], 1);
-    Serial.println("% of factory value");
-
-    // Calibrate gyro and accelerometers, load biases in bias registers
-    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
-
-    myIMU.initMPU9250();
-    // Initialize device for active mode read of acclerometer, gyroscope, and
-    // temperature
-    Serial.println("MPU9250 initialized for active data mode....");
-
-    // Read the WHO_AM_I register of the magnetometer, this is a good test of
-    // communication
-    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
-    Serial.print("AK8963 ");
-    Serial.print("I AM 0x");
-    Serial.print(d, HEX);
-    Serial.print(" I should be 0x");
-    Serial.println(0x48, HEX);
-
-    if (d != 0x48) {
-      // Communication failed, stop here
-      Serial.println(F("Communication failed, abort!"));
-      Serial.flush();
-      abort();
-    }
-
-    // Get magnetometer calibration from AK8963 ROM
-    myIMU.initAK8963(myIMU.factoryMagCalibration);
-    // Initialize device for active mode read of magnetometer
-    Serial.println("AK8963 initialized for active data mode....");
-
-    //  Serial.println("Calibration values: ");
-    Serial.print("X-Axis factory sensitivity adjustment value ");
-    Serial.println(myIMU.factoryMagCalibration[0], 2);
-    Serial.print("Y-Axis factory sensitivity adjustment value ");
-    Serial.println(myIMU.factoryMagCalibration[1], 2);
-    Serial.print("Z-Axis factory sensitivity adjustment value ");
-    Serial.println(myIMU.factoryMagCalibration[2], 2);
-
-    // Get sensor resolutions, only need to do this once
-    myIMU.getAres();
-    myIMU.getGres();
-    myIMU.getMres();
-  }
-}
+MPU9250 imu(Wire, 0x68);
 
 void setup() {
   SerialUSB.begin(115200);
-  while (!SerialUSB)
-    ;
+  // while (!SerialUSB)
+  //   ;
   delay(500);
   delay(5500);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  InitIMU(SerialUSB, myIMU);
-
-  int status = -1;
+  int imu_status = imu.begin();
+  if (imu_status <= 0) {
+    SerialUSB.println("IMU failed. Status: " + String(imu_status));
+  }
 }
 
 void loop() {
   // IMU.readSensor();
   SerialUSB.println("loop");
-  // byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
-  // SerialUSB.print(F("MPU9250 I AM 0x"));
-  // SerialUSB.print(c, HEX);
-  // SerialUSB.print(F(" I should be 0x"));
-  // SerialUSB.println(0x71, HEX);
+  imu.readSensor();
+
+  float magX = imu.getMagX_uT();
+  float magY = imu.getMagY_uT();
+  float magZ = imu.getMagZ_uT();
 
   //   // printScaledAGMT(myICM.agmt);
-  // float magX = IMU.getMagX_uT();
-  // float magY = IMU.getMagY_uT();
-  // float magZ = IMU.getMagZ_uT();
 
-  // SerialUSB.print(" ], Mag (uT) [ ");
-  // printFormattedFloat(magX, 5, 2);
-  // SerialUSB.print(", ");
-  // printFormattedFloat(magY, 5, 2);
-  // SerialUSB.print(", ");
-  // printFormattedFloat(magZ, 5, 2);
-  // SerialUSB.print(" ]");
-  // SerialUSB.println();
+  SerialUSB.print(" ], Mag (uT) [ ");
+  printFormattedFloat(magX, 5, 2);
+  SerialUSB.print(", ");
+  printFormattedFloat(magY, 5, 2);
+  SerialUSB.print(", ");
+  printFormattedFloat(magZ, 5, 2);
+  SerialUSB.print(" ]");
+  SerialUSB.println();
 
-  // auto direction = get_direction(magX, magY, magZ);
-  // SerialUSB.println(direction);
-  // delay(30);
+  auto direction = get_direction(magX, magY, magZ);
+  SerialUSB.println(direction);
 
   delay(2000);
 }
