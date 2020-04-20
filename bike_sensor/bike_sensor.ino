67b6c2e76300c47fe0e84bb0b666b3385859db01
Nam Ngo <nam.ngo.shop@gmail.com>
2020-04-19 18:53:07 -0700

    Try sparkfun dmp

--- bike_sensor/bike_sensor.ino
+++ bike_sensor/bike_sensor.ino
@@ -1,18 +1,61 @@
-#include <MPU9250.h>
+
+#include <Arduino.h>
+// #include <Esp.h>
+#include <SparkFunMPU9250-DMP.h>
 #include <Wire.h>
+
+#include "EEPROM.h"
+
+// #include <sstream>
+// #include <string>
 
 #define SDA_PIN 21
 #define SCL_PIN 22
 #define MPU_INT_PIN 19
-#define LED_BUILTIN 16
+
+MPU9250_DMP imu;
+
+/* EEPROM buffer to mag bias and scale factors */
+uint8_t eeprom_buffer[24];
+float value;
 
 void setup() {
   Serial.begin(115200);
   delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
 
   Wire.begin(SDA_PIN, SCL_PIN);
-  Serial.println("init");
+
+  while (imu.begin() != INV_SUCCESS) {
+    Serial.println("Unable to communicate with MPU-9250");
+    Serial.println("Check connections, and try again.");
+    Serial.println();
+    delay(5000);
+  }
+  imu.setSensors(INV_XYZ_GYRO | INV_XYZ_ACCEL | INV_XYZ_COMPASS);
+  // Use setGyroFSR() and setAccelFSR() to configure the
+  // gyroscope and accelerometer full scale ranges.
+  // Gyro options are +/- 250, 500, 1000, or 2000 dps
+  imu.setGyroFSR(2000);  // Set gyro to 2000 dps
+  // Accel options are +/- 2, 4, 8, or 16 g
+  imu.setAccelFSR(2);  // Set accel to +/-2g
+  // Note: the MPU-9250's magnetometer FSR is set at
+  // +/- 4912 uT (micro-tesla's)
+
+  // setLPF() can be used to set the digital low-pass filter
+  // of the accelerometer and gyroscope.
+  // Can be any of the following: 188, 98, 42, 20, 10, 5
+  // (values are in Hz).
+  imu.setLPF(5);  // Set LPF corner frequency to 5Hz
+
+  // The sample rate of the accel/gyro can be set using
+  // setSampleRate. Acceptable values range from 4Hz to 1kHz
+  imu.setSampleRate(10);  // Set sample rate to 10Hz
+
+  // Likewise, the compass (magnetometer) sample rate can be
+  // set using the setCompassSampleRate() function.
+  // This value can range between: 1-100Hz
+  imu.setCompassSampleRate(10);  // Set mag rate to 10Hz
+
   // Wire.setClock(400000);
   // Serial.println("begining bme280");
   // if (!bme.begin(BME280_ADDR, &Wire)) {
@@ -28,27 +71,20 @@ void setup() {
   //   Serial.print("        ID of 0x61 represents a BME 680.\n");
   // }
   // Serial.println("done bme280");
-
-  // while (!icm_init) {
-  //   Serial.println("begining icm");
-  //   myICM.begin(Wire, AD0_VAL);
-  //   Serial.println(myICM.statusString());
-  //   if (myICM.status != ICM_20948_Stat_Ok) {
-  //     Serial.println("Trying again...");
-  //     delay(500);
-  //   } else {
-  //     icm_init = true;
-  //     Serial.println("init!");
-  //   }
-  // }
 }
 
 void loop() {
   // IMU.readSensor();
-  Serial.println("blink");
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
+  if (imu.dataReady()) {
+    // Call update() to update the imu objects sensor data.
+    // You can specify which sensors to update by combining
+    // UPDATE_ACCEL, UPDATE_GYRO, UPDATE_COMPASS, and/or
+    // UPDATE_TEMPERATURE.
+    // (The update function defaults to accel, gyro, compass,
+    //  so you don't have to specify these values.)
+    imu.update(UPDATE_ACCEL | UPDATE_GYRO | UPDATE_COMPASS);
+    printIMUData();
+  }
 
   // //   // printScaledAGMT(myICM.agmt);
   // float magX = IMU.getMagX_uT();
@@ -68,7 +104,38 @@ void loop() {
   // Serial.println(direction);
   // delay(30);
 
-  delay(500);
+  delay(2500);
+}
+
+void printIMUData(void) {
+  // After calling update() the ax, ay, az, gx, gy, gz, mx,
+  // my, mz, time, and/or temerature class variables are all
+  // updated. Access them by placing the object. in front:
+
+  // Use the calcAccel, calcGyro, and calcMag functions to
+  // convert the raw sensor readings (signed 16-bit values)
+  // to their respective units.
+  float accelX = imu.calcAccel(imu.ax);
+  float accelY = imu.calcAccel(imu.ay);
+  float accelZ = imu.calcAccel(imu.az);
+  float gyroX = imu.calcGyro(imu.gx);
+  float gyroY = imu.calcGyro(imu.gy);
+  float gyroZ = imu.calcGyro(imu.gz);
+  float magX = imu.calcMag(imu.mx);
+  float magY = imu.calcMag(imu.my);
+  float magZ = imu.calcMag(imu.mz);
+
+  Serial.println("Accel: " + String(accelX) + ", " + String(accelY) + ", " +
+                 String(accelZ) + " g");
+  Serial.println("Gyro: " + String(gyroX) + ", " + String(gyroY) + ", " +
+                 String(gyroZ) + " dps");
+  Serial.println("Mag: " + String(magX) + ", " + String(magY) + ", " +
+                 String(magZ) + " uT");
+  // auto direction = get_direction(magX, magY, magZ);
+  auto direction = get_direction(-11.3, 14.3, -47.4);
+  Serial.println(direction);
+  Serial.println("Time: " + String(imu.time) + " ms");
+  Serial.println();
 }
 
 void printFormattedFloat(float val, uint8_t leading, uint8_t decimals) {
@@ -103,9 +170,9 @@ String get_direction(float x, float y, float z) {
   int heading = 0;
 
   if (y > 0) {
-    heading = 90 - atan(x / y) * RAD_TO_DEG;
+    heading = 90 - atan(x / y) * (180 / M_PI);
   } else if (y < 0) {
-    heading = 270 - atan(x / y) * RAD_TO_DEG;
+    heading = 270 - atan(x / y) * (180 / M_PI);
   } else if (y == 0 && x < 0) {
     heading = 180;
   } else if (y == 0 && x > 0) {
